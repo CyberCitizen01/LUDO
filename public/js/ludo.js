a2c9f58d0369e5fea9a4c1313db11493dd29fd9b
CyberCitizen01 <mr.20u10448@btech.nitdgp.ac.in>
2021-05-19 23:00:14 +0530

    Pieces load now
    Now pieces of various player are able to be drawn on the canvas for all four players, even if someone enter the room after.

--- public/js/ludo.js
+++ public/js/ludo.js
@@ -2,27 +2,64 @@ let socket = io(window.location.href.substring(0,window.location.href.length-7))
 
 var canvas = document.getElementById('theCanvas');
 var ctx = canvas.getContext('2d');
-
 canvas.height = 750;
 canvas.width = 750;
 
-var greenPiece = new Image(); greenPiece.src = "../images/pieces/green.png"
-var bluePiece = new Image(); bluePiece.src = "../images/pieces/blue.png"
-var redPiece = new Image(); redPiece.src = "../images/pieces/red.png"
-var yellowPiece = new Image(); yellowPiece.src = "../images/pieces/yellow.png"
+// var greenPiece = new Image(); greenPiece.src = "../images/pieces/green.png"
+// var bluePiece = new Image(); bluePiece.src = "../images/pieces/blue.png"
+// var redPiece = new Image(); redPiece.src = "../images/pieces/red.png"
+// var yellowPiece = new Image(); yellowPiece.src = "../images/pieces/yellow.png"
 
-greenPiece.onload = ()=>{
-    ctx.drawImage(greenPiece, 72, 123.5);
-    ctx.drawImage(greenPiece, 122, 173.5);
-    ctx.drawImage(greenPiece, 122, 73.5);
-    ctx.drawImage(greenPiece, 172, 123.5);
-}
+// greenPiece.onload = ()=>{
+//     ctx.drawImage(greenPiece, 72, 123.5);
+//     ctx.drawImage(greenPiece, 122, 173.5);
+//     ctx.drawImage(greenPiece, 122, 73.5);
+//     ctx.drawImage(greenPiece, 172, 123.5);
+// }
 
 //context.clearRect(0, 0, canvas.width, canvas.height);
-let room_code = window.location.href.substring(window.location.href.length-6);
-let MYROOM = [];
+const room_code = window.location.href.substring(window.location.href.length-6);
+const USERNAMES = ['Green Warrior', 'Yellow Rhino', 'Blue Fox', 'Red Fire'];
+const MYROOM = [];
+const PIECES = [];
+const PLAYERS = [];
+const colors = ["green","yellow","blue","red"];
 let myid = -1;
-let USERNAMES = ['Green Warrior', 'Yellow Rhino', 'Blue Fox', 'Red Fire'];
+
+let allPiecesePos = {
+    0:[{x: 72,y:123.5},{x:122,y: 73.5},{x:172,y:123.5},{x:122,y:173.5}],
+    1:[{x: 72,y:573.5},{x:122,y:523.5},{x:172,y:573.5},{x:122,y:623.5}],
+    2:[{x:522,y:573.5},{x:572,y:523.5},{x:622,y:573.5},{x:572,y:623.5}],
+    3:[{x:522,y:123.5},{x:572,y: 73.5},{x:622,y:123.5},{x:572,y:173.5}]
+}
+
+class Player{
+    constructor(id){
+        this.id = id;
+        this.myPieces = new Array();
+        for(let i=0;i<4;i++){
+            this.myPieces.push(new Piece(i,this.id))
+        }        
+    }
+    draw(){
+        for(let i=0;i<4;i++){
+            this.myPieces[i].draw();
+        }
+    }
+}
+
+class Piece{
+    constructor(i,id){
+        this.parrentid = id
+        this.x = allPiecesePos[this.parrentid][i].x
+        this.y = allPiecesePos[this.parrentid][i].y
+        this.image = PIECES[id]
+    }
+
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y);
+    }
+}
 
 socket.on('connect',function(){
     console.log('You are connected to the server!!');
@@ -30,20 +67,20 @@ socket.on('connect',function(){
     socket.on('refresh',()=>{location.reload()});
     socket.emit('fetch',room_code,function(data,id){
         console.log('hihello');
-        MYROOM = data;
+        for(let i=0;i<data.length;i++){
+            MYROOM.push(data[i]);
+        }
         myid = id;
-        iStartedTheGame();
-        MYROOM.forEach(function(numb){numb==myid?outputMessage('You',0):outputMessage(USERNAMES[numb],0)});
-        document.getElementById('my-name').innerHTML += USERNAMES[myid]
-        styleButton(0);
+        StartTheGame();
     });
     //socket.on('H',(data,cb)=>{console.log(data);cb()})
 //To simulate dice
-    diceAction();
+    //diceAction();
     
 
     socket.on('new-user-joined',function(data){
         MYROOM.push(data.id);
+        loadNewPiece(data.id);
         outputMessage(USERNAMES[data.id],0);
     });
 
@@ -105,8 +142,8 @@ function diceAction(){
         console.log('clicked Random Button');
         let myTurn = {
             room: room_code,
-            Name: USERNAMES[myid],
-            Num: Math.floor((Math.random() * 6) + 1)
+            Name: USERNAMES[myid],                      //need to 
+            Num: Math.floor((Math.random() * 6) + 1)    //make changes here!!
         }
         socket.emit('random',myTurn, function(){
             outputMessage({Name: 'you', Num:myTurn.Num},1);
@@ -115,8 +152,58 @@ function diceAction(){
     });
 }
 
-function iStartedTheGame(){
-    if(MYROOM.length===1){
-        document.getElementById('i-started-the-game').style.display = "block"
+function StartTheGame(){
+    MYROOM.forEach(function(numb){
+        numb==myid?outputMessage('You',0):outputMessage(USERNAMES[numb],0)
+    });
+    document.getElementById('my-name').innerHTML += USERNAMES[myid]
+    if(MYROOM.length === 1){
+        styleButton(1);
+    }else{
+        styleButton(0);
+    }
+    loadAllPieces();
+}
+
+function loadAllPieces(){
+    let cnt = 0;
+    for(let i=0;i<colors.length;i++){
+        let img = new Image();
+        img.src = "../images/pieces/"+colors[i]+".png";
+        img.onload = ()=>{
+            ++cnt;
+            if(cnt >= colors.length){
+                //all images are loaded
+                for(let j=0;j<MYROOM.length;j++){
+                    PLAYERS.push(new Player(MYROOM[j]));
+                }
+                allPlayerHandler();
+            }
+        }
+        PIECES.push(img);
+    }
+}
+
+function chanceRotation(id){
+    if(id+1 >= 4){
+        return 0;
+    }else{
+        return id+1
+    }
+}
+
+function allPlayerHandler(){
+    for(let i=0;i<PLAYERS.length;i++){
+        PLAYERS[i].draw();
+    }
+}
+
+function loadNewPiece(id){
+    //Yeah load some new pieces bruuh!!
+    let img = new Image();
+    img.src = "../images/pieces/"+colors[id]+".png";
+    img.onload = ()=>{
+        PLAYERS.push(new Player(id));
+        allPlayerHandler();
     }
 }
