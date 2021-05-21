328736ab57d540c1e4d10c327b9eb205d410f3cf
CyberCitizen01 <mr.20u10448@btech.nitdgp.ac.in>
2021-05-22 00:44:16 +0530

    'first six to move' is working
    fixed generate_member_id, also fixed the bug when more than 4 people try to join the same room, added dice stimulation, and first six to movegit diff server/server.js

--- server/server.js
+++ server/server.js
@@ -17,18 +17,18 @@ let USERNAMES = ['Green Warrior', 'Yellow Rhino', 'Blue Fox', 'Red Fire'];
 let rooms = {};
 let messages = {};
 
+
+//
+///sockets
+//
 let nsp = io.of('/ludo');
 
 nsp.on('connection',(socket)=>{
     console.log('A User has connected to the game');
-    //socket.emit('H',"HHHH",()=> console.log("hello"))
-    //socket.on('testestest',(data,cb)=>{console.log(data);cb();});
     socket.on('fetch',(data,cb)=>{
         try{
-            let member_id = generate_member_id(socket,data);
-            console.log(rooms);
+            let member_id = generate_member_id(socket.id,data);
             socket.join(data);
-            //console.log(socket);
             if(member_id !== -1){
                 cb(Object.keys(rooms[data]),member_id);
                 socket.to(data).emit('new-user-joined',{id:member_id});
@@ -37,31 +37,38 @@ nsp.on('connection',(socket)=>{
             }
         }
         catch(err){
-            console.log(rooms);
             if(err.name === 'TypeError'){
                 socket.emit('imposter');
             }
-            console.log("hello",err);
+            console.log("hello",err,rooms);
         }
     });
 
-    socket.on('disconnect',()=>{
-        console.log('That client just got disconnected-',socket.id);
-        deleteThisid(socket);
-        //delete rooms[data.room][data.id];
+    socket.on('chance',(data)=>{
+        nsp.to(data.room).emit('is-it-your-chance',data.nxt_id);
     });
-    // nsp.on('random',(data,cb)=>{
-    //     socket.to(data.room).emit('otherUserData',{Name: data.Name, Num: data.Num});
-    //     cb();
-    // });
+
+    socket.on('random',(data,cb)=>{
+        let temp = Math.floor((Math.random()*6) + 1);
+        socket.to(data.room).emit('Thrown-dice', {id:data.id, Num:temp});
+        cb(temp);
+    });
+
+    socket.on('disconnect',()=>{
+        deleteThisid(socket.id);
+        console.log('A client just got disconnected');
+    });
 });
 
+
+//
+///Routes management
+//
 app.get('/', (req,res)=>{
     res.sendFile('index.html', { root: publicPath + '/html' });
 });
 
 app.post('/',(req,res)=>{
-    // console.log(req.body.action_to_do);
     if(req.body.action_to_do === 'create'){
         let p0th = randomPath()
         rooms[p0th] = {};
@@ -84,7 +91,6 @@ app.get('/ludo', (req,res)=>{
 });
 
 app.get('/ludo/:ROOMCODE', (req,res)=>{
-    console.log(req.params.ROOMCODE,req.query);
     if(Object.keys(rooms).includes(req.params.ROOMCODE) && Object.keys(req.query).length===0  &&  Object.keys(rooms[req.params.ROOMCODE]).length <= 3){
         res.sendFile('ludo.html', { root: publicPath + '/html' });
     } else{
@@ -94,7 +100,6 @@ app.get('/ludo/:ROOMCODE', (req,res)=>{
 });
 
 app.use(function (req, res) {
-    console.log(req.url,req.originalUrl,typeof(req.url),typeof(req.originalUrl));
     res.statusCode = 404;
     res.end('404!');
 });
@@ -103,6 +108,12 @@ server.listen(port,()=>{
     console.log(`The server has started working on http://localhost:${port}`);
 })
 
+
+//
+///CUSTOM FUNCTIONS
+//
+
+//to generate unique rooms
 function randomPath(){
     let randomPath = Math.random().toString(36).substr(2, 6);
     if(!Object.keys(rooms).includes(randomPath)){
@@ -110,23 +121,30 @@ function randomPath(){
     } else{ randomPath(); }
 };
 
+//to randomise the color a player can get when he 'fetch'es.
 function generate_member_id(s_id,rc){
     let m_id = Math.floor(Math.random()*4);
     let m_r = Object.keys(rooms[rc]);
     if(m_r.length <= 4){
         if(m_r.includes(m_id.toString())){
             return generate_member_id(s_id,rc)
+        }else{
+            rooms[rc][m_id] = s_id;
+            return m_id;
         }
     } else{
         return -1;
     }
 }
 
-function deleteThisid(socket){
+//to delete the extra place when (only one) user refreshes.
+function deleteThisid(id){
     for(var roomcd in rooms){
         if(rooms.hasOwnProperty(roomcd)){
-            ky = Object.keys(rooms[roomcd]).find( key => rooms[roomcd][key] === socket.id);
-            delete rooms[roomcd][ky];
+            ky = Object.keys(rooms[roomcd]).find( key => rooms[roomcd][key] === id);
+            if(typeof(ky) === 'string'){
+                delete rooms[roomcd][ky];
+            }
         }
     }
 }
